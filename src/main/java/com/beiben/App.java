9d794645354f7cd09423764672054da27209b3e3
beiben <grant_ding@aliyun.com>
2021-03-08 23:23:23 +0800

    Question 131 part1

--- src/main/java/com/beiben/App.java
+++ src/main/java/com/beiben/App.java
@@ -1,13 +1,27 @@
 package com.beiben;
 
+import com.beiben.leetcode.solution131.Solution;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
 
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        ObjectMapper om = new ObjectMapper();
+        log.info("start solution131");
+        Solution solution = new Solution();
+        try {
+            log.info(om.writeValueAsString(solution.partition("aab")));
+            log.info(om.writeValueAsString(solution.partition("a")));
+        } catch (Exception ex) {
+            log.error(ex.getMessage(), ex);
+        }
+        log.info("end solution131");
     }
 }
--- src/main/java/com/beiben/leetcode/solution131/Solution.java
+++ src/main/java/com/beiben/leetcode/solution131/Solution.java
@@ -1,9 +1,61 @@
 package com.beiben.leetcode.solution131;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
+
+    List<List<String>> result = new ArrayList<>();
+
+    String origin;
+
     public List<List<String>> partition(String s) {
-        return null;
+        origin = s;
+        List<String> palindromes = new ArrayList<>();
+        dividePartition(palindromes, s);
+        return result;
     }
+
+    private boolean isPalindrome(String s) {
+        for (int i = 0; i <= s.length() / 2; i++) {
+           if (!s.substring(i, 1).equals(s.substring(s.length() - 1 - i, 1))) {
+               return false;
+           }
+        }
+        return true;
+
+    }
+
+    private void dividePartition(List<String> palindromes, String s) {
+        if (isPalindrome(s)) {
+            palindromes.add(s);
+            if (origin.equals(join(palindromes))) {
+                result.add(palindromes);
+                if (s.length() == 1) {
+                    return;
+                } else {
+                    List<String> newPalindromes = new ArrayList<>();
+                    newPalindromes.addAll(palindromes.subList(0, palindromes.size() - 1));
+                    newPalindromes.add(s.substring(0, 1));
+                    dividePartition(newPalindromes, s.substring(1));
+                }
+            }
+        } else {
+            if (palindromes.size() > 0) {
+                String last = palindromes.remove(palindromes.size() - 1);
+                dividePartition(palindromes, last + s);
+            } else {
+                return;
+            }
+        }
+    }
+
+    private String join(List<String> palindromes) {
+        StringBuilder sb = new StringBuilder();
+        for (String s : palindromes) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
 }
