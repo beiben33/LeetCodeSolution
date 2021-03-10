7562595126ff6d9749809000bfe3949e9e4652b8
beiben <grant_ding@aliyun.com>
2021-03-10 17:06:26 +0800

    add official solution of question 131

--- src/main/java/com/beiben/App.java
+++ src/main/java/com/beiben/App.java
@@ -1,6 +1,6 @@
 package com.beiben;
 
-import com.beiben.leetcode.solution131.Solution;
+import com.beiben.leetcode.solution131.StandardSolution;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,10 +15,11 @@ public class App {
     public static void main( String[] args ) {
         ObjectMapper om = new ObjectMapper();
         log.info("start solution131");
-        Solution solution = new Solution();
         try {
-            log.info(om.writeValueAsString(solution.partition("aab")));
-            log.info(om.writeValueAsString(solution.partition("a")));
+            StandardSolution standardSolution = new StandardSolution();
+            log.info(om.writeValueAsString(standardSolution.partition("aab")));
+            standardSolution = new StandardSolution();
+            log.info(om.writeValueAsString(standardSolution.partition("a")));
         } catch (Exception ex) {
             log.error(ex.getMessage(), ex);
         }
