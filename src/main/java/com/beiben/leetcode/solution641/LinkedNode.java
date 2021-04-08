f4dc4c2c36106f8ed4aac941acb7c8d7d64c65be
beiben <grant_ding@aliyun.com>
2021-04-08 16:55:22 +0800

    add solution of question 641 (Design Circular Deque)

---
+++ src/main/java/com/beiben/leetcode/solution641/LinkedNode.java
@@ -0,0 +1,11 @@
+package com.beiben.leetcode.solution641;
+
+public class LinkedNode {
+    int value;
+    LinkedNode prev;
+    LinkedNode next;
+
+    LinkedNode(int value) {
+        this.value = value;
+    }
+}
